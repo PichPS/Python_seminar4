7015d42d423ae5f6538ee543dc14467f6a5bf121
Pavel_Pichugin <pichps@mail.ru>
2023-05-30 16:16:28 +0300

    less27 - finish

---
+++ less27.py
@@ -0,0 +1,16 @@
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea shore shells
+# Output: 13
+
+# print(len(set(input("Введите текст: ").lower().split())))
+word = input("Введите текст: ")
+word = word.lower()
+word = word.split()
+result = []
+for i in word:
+    if i not in result:
+        result.append(i)
+print(len(result))
